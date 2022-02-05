57aa0b2f9b3053df8084b529606d4b7e14a27429
Seungpill Han <gkstmdvlf2@gmail.com>
2022-02-05 11:04:21 +0900

    chapter3_Css Style

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
+//Shift + option + f : 소스코드 자동정렬
+
 function ExpenseItem() {
-    return <h2> Expense Item!</h2>
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <h2>Car Insurance</h2>
+      <div>$294.67</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
