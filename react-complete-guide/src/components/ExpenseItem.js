28163425ebe11096f4307e34a9b1f6c2776db1e7
SeungpilHan <gkstmdvlf2@gmail.com>
2022-02-06 13:29:59 +0900

    Chapter3_JSX_Css

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,11 +1,18 @@
 //Shift + option + f : 소스코드 자동정렬
+import './ExpenseItem.css'
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 2, 6);
+  const expenseTitle = 'Car Insurance'
+  const expenseAmount = 294.67;
+
   return (
-    <div>
-      <div>March 28th 2021</div>
-      <h2>Car Insurance</h2>
-      <div>$294.67</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+      <h2>{expenseTitle}</h2>
+      <div className="expense-item__price">${expenseAmount}</div>
+      </div>
     </div>
   );
 }
