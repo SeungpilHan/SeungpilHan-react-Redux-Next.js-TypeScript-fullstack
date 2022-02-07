24082ccce470fb3e18faa5fb6553fce6215a91cc
SeungpilHan <gkstmdvlf2@gmail.com>
2022-02-07 14:06:53 +0900

    mac commit

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 //Shift + option + f : 소스코드 자동정렬
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 2, 6);
-  const expenseTitle = 'Car Insurance'
-  const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-      <h2>{expenseTitle}</h2>
-      <div className="expense-item__price">${expenseAmount}</div>
+      <h2>{props.title}</h2>
+      <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
