8b2ce159f76869e3a40def913222824f562916a0
SeungpilHan <gkstmdvlf2@gmail.com>
2022-02-09 19:34:25 +0900

    component

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,18 +1,13 @@
 //Shift + option + f : 소스코드 자동정렬
-import './ExpenseItem.css'
+import './ExpenseDate';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>      
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate/>
       <div className="expense-item__description">
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
